db208db19ac2df5064d88e5bdbbbfc27ebef4b06
Jason <jason.y.jeong@gmail.com>
2020-09-17 15:42:26 -0400

    clean

--- nycbnb.py
+++ nycbnb.py
@@ -28,12 +28,56 @@ nyc.fillna({'reviews_per_month':0}, inplace=True)
 
 
 
+
+
+
+
+
+
+#NEIGHBOURHOOD BREAKDOWN
+
 #DIFFERENT NEIGHBOURHOODS
 print(nyc.neighbourhood_group.unique())
+
+#BAR GRAPH OF ROOM TYPE BY NEIGHBOURHOOD
+plt.figure(figsize=(10,6))
+sns.countplot(x = 'room_type',hue = "neighbourhood_group",data = nyc)
+plt.title("Room Types Occupied By Neighbourhood")
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
 
 
 #DIFFERENT ROOM TYPES
 print(nyc.room_type.unique())
+
+#ROOM TYPE BREAKDOWN
+plt.style.use('fivethirtyeight')
+f,ax=plt.subplots(1,2,figsize=(18,8))
+nyc['room_type'].value_counts().plot.pie(explode=[0,0.05,0],autopct='%1.1f%%',ax=ax[0],shadow=True)
+ax[0].set_title('Room Type')
+ax[0].set_ylabel('Room Type')
+sns.countplot('room_type',data=nyc,ax=ax[1],order=nyc['room_type'].value_counts().index)
+ax[1].set_title('Room Type')
+plt.show()
+
+
+
+
+
+
+
+
+
 
 
 #PRINT TOP HOSTS' ID
@@ -69,35 +113,6 @@ def rank_price(hotel_price):
 nyc['price'].apply(rank_price).value_counts().plot(kind='bar');
 
 
-
-
-
-
-
-
-
-#ROOM TYPE BREAKDOWN
-plt.style.use('fivethirtyeight')
-f,ax=plt.subplots(1,2,figsize=(18,8))
-nyc['room_type'].value_counts().plot.pie(explode=[0,0.05,0],autopct='%1.1f%%',ax=ax[0],shadow=True)
-ax[0].set_title('Room Type')
-ax[0].set_ylabel('Room Type')
-sns.countplot('room_type',data=nyc,ax=ax[1],order=nyc['room_type'].value_counts().index)
-ax[1].set_title('Room Type')
-plt.show()
-
-
-
-
-
-
-
-
-#BAR GRAPH OF ROOM TYPE BY NEIGHBOURHOOD
-plt.figure(figsize=(10,6))
-sns.countplot(x = 'room_type',hue = "neighbourhood_group",data = nyc)
-plt.title("Room Types Occupied By Neighbourhood")
-plt.show()
 
 
 
